e5d9bcc15f5c3215b7d57b9b34ee07e5c741cac6
bartuozkanay <bartuozkanay1@gmail.com>
2025-04-07 22:31:10 +0300

    Update Untitled-2.py

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,44 +1,28 @@
 
+import streamlit as st
+
 latince_arapca = {
-    "a": "ا",
-    "b": "ب",
-    "c": "ج",
-    "ç": "چ",
-    "d": "د",
-    "e": "e",
-    "f": "ف",
-    "g": "گ",
-    "ğ": "غ",
-    "h": "ه",
-    "ı": "ی",   
-    "i": "ی",
-    "j": "ژ",
-    "k": "ك",
-    "l": "ل",
-    "m": "م",
-    "n": "ن",
-    "o": "و",
-    "ö": "ۆ",   
-    "p": "پ",
-    "r": "ر",
-    "s": "س",
-    "ş": "ش",
-    "t": "ت",
-    "u": "و",
-    "ü": "ۈ",
-    "v": "ڤ",
-    "y": "ی",
-    "z": "ز",
-    " ": " "  }
+    "a": "ا", "b": "ب", "c": "ج", "ç": "چ", "d": "د", "e": "e", "f": "ف",
+    "g": "گ", "ğ": "غ", "h": "ه", "ı": "ی", "i": "ی", "j": "ژ", "k": "ك",
+    "l": "ل", "m": "م", "n": "ن", "o": "و", "ö": "ۆ", "p": "پ", "r": "ر",
+    "s": "س", "ş": "ش", "t": "ت", "u": "و", "ü": "ۈ", "v": "ڤ", "y": "ی",
+    "z": "ز", " ": " "
+}
 
 def latince_osmanlica(text):
     result = ""
     for char in text.lower():
         result += latince_arapca.get(char, char)  
-    return result[::-1]
+    return result[::-1]  
 
-if 1 == 1:
-    print("Latince-Osmanlıca Dönüştürücü")
-    girdi = input("lütfen Çevirmek istediğiniz Latince yazıyı girin")
-    yeni_metin = latince_osmanlica(girdi)
-    print(yeni_metin)
+st.title("Latince - Osmanlıca Çevirici")
+
+
+girdi = st.text_input("Latince metni giriniz:")
+
+if girdi:
+    yeni = latince_osmanlica(girdi)
+    st.subheader("Osmanlıca Çeviri:")
+    st.write(yeni)
+else:
+    st.write("Lütfen çevirmek istediğiniz Latince metni giriniz.")  # Prompt for input
